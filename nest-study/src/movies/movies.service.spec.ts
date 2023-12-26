6125041787bec3010eb826935e9be43cc4e991ff
yuseonkim <yusun715@icloud.com>
2023-12-26 16:02:22 +0900

    feat : 유닛 테스트 작성 movieService 관련 유닛테스트 작성

--- nest-study/src/movies/movies.service.spec.ts
+++ nest-study/src/movies/movies.service.spec.ts
@@ -17,7 +17,72 @@ describe('MoviesService', () => {
       expect(service).toBeDefined();
     }); 
 
-    it("should be 4", () => {
-        expect(2+2).toEqual(4)
-    })
+    describe("getAll", () => {
+        it("should return an array", () => {
+            const result = service.getAll();
+            expect(result).toBeInstanceOf(Array);
+        });
+    });
+
+    describe("getOne",() => {
+        it("should return an Movie",()=>{
+            service.create({
+                title:'Test Movie',
+                year: 2000,
+                genres:['test']
+            });
+            const movie = service.getOne(1);
+            expect(movie).toBeDefined();
+            expect(movie.id).toEqual(1);
+
+        });
+
+        it("should throw 404 error",()=>{
+            try{
+                service.getOne(999);
+            }catch(e){
+                expect(e).toBeInstanceOf(NotFoundException);
+                expect(e.message).toEqual('Movie with ID 999 not found.')
+            }
+        });
+    });
+
+    describe("deleteOne",()=>{
+        it("delete a movie",()=>{
+            service.create({
+                title : 'a',
+                genres : ['a'],
+                year : 2000
+            });
+            const beforeDelete = service.getAll();
+            service.deleteOne(1);
+            const afterDelete = service.getAll();
+            expect(afterDelete.length).toBeLessThan(beforeDelete.length);
+        });
+
+        it("should return a 404",()=>{
+            try{
+                service.deleteOne(999);
+        }catch(e){
+            expect(e).toBeInstanceOf(NotFoundException);
+            expect(e.message).toEqual('Movie with ID 999 not found.')
+        };
+        });
+    });
+
+    describe("create",()=>{
+        it("should create a movie",()=>{
+            const beforeCreate = service.getAll.length;
+            service.create({
+                title : 'a',
+                genres : ['a'],
+                year : 2000
+            });
+
+            const afterCreate = service.getAll().length;
+            expect(afterCreate).toBeGreaterThan(beforeCreate);
+        });
+
+        
+    });
 });
